12cf1da5c6d539a60c958b7e59c9aa4905f42cc7
Nicholas <142807894+Nicholas-J-S-Pitts@users.noreply.github.com>
2024-09-13 10:14:32 -0300

    Add files via upload

---
+++ ConsumerViaCep/Program.cs
@@ -0,0 +1,18 @@
+﻿using static System.Console;
+
+WriteLine("Digite o seu CEP: ");
+var cep= ReadLine();
+
+WriteLine("Realizando aquisição na API... ");
+var enderecoUrl = $@"https://viacep.com.br/ws/{cep}/json/";
+var cliente = new HttpClient();
+try {
+    HttpResponseMessage? resposta = await cliente.GetAsync(enderecoUrl);
+    resposta.EnsureSuccessStatusCode();
+    string respostaApiString = await resposta.Content.ReadAsStringAsync();
+    WriteLine(respostaApiString);
+} 
+catch (SystemException) 
+{
+    WriteLine("Erro da Api: " + e);
+}
